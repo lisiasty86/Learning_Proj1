3b05e1bc94aaf452c6ad65ed27386b1f6e5006f8
LLIS <lisiasty86@gmail.com>
2019-05-17 15:26:34 +0200

    Setting up1

--- src/test/java/Learning1/Steps/ContactUs.java
+++ src/test/java/Learning1/Steps/ContactUs.java
@@ -1,22 +1,54 @@
 package Learning1.Steps;
 
-import cucumber.api.java8.En;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ContactUs implements En {
-    public ContactUs() {
-        Given("^I access webdriveruniversity contact us form$", () -> {
-        });
-        When("^I enter a valid first name$", () -> {
-        });
-        When("^I enter a valid last name$", () -> {
-        });
-        And("^I enter a valid email address$", () -> {
-        });
-        And("^I enter comments(\\d+)$", (Integer arg0) -> {
-        });
-        When("^I click on the submit button(\\d+)$", (Integer arg0) -> {
-        });
-        Then("^the information should successfully be submitted via the contact us form(\\d+)$", (Integer arg0) -> {
-        });
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ContactUs {
+    WebDriver driver;
+
+    @Before()
+    public void setup() throws IOException {
+        //System.setProperty("webdriver.chrome.driver\", \"C:\\\\Users\\\\llis\\\\Desktop\\\\MOJE\\\\PROGRAMOWANIE\\\\chromedriver.exe");
+        this.driver = new ChromeDriver();
+        this.driver.manage().window().maximize();
+        this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+    }
+
+    @Given("I access webdriveruniversity contact us form")
+    public void iAccessWebdriveruniversityContactUsForm() {
+    }
+
+    @When("I enter a valid first name")
+    public void iEnterAValidFirstName() {
+    }
+
+    @When("^I enter a valid last name$")
+    public void iEnterAValidLastName() {
+    }
+
+    @And("I enter a valid email address")
+    public void iEnterAValidEmailAddress() {
+    }
+
+    @And("I enter comments{int}")
+    public void iEnterComments(int arg0) {
+
+    }
+
+    @When("I click on the submit button{int}")
+    public void iClickOnTheSubmitButton(int arg0) {
+
+    }
+
+    @Then("the information should successfully be submitted via the contact us form{int}")
+    public void theInformationShouldSuccessfullyBeSubmittedViaTheContactUsForm(int arg0) {
     }
 }
--- src/test/java/Learning1/testRunner/mainRunner.java
+++ src/test/java/Learning1/testRunner/mainRunner.java
@@ -1,17 +1,16 @@
-/*package Learning1.testRunner;
+package Learning1.testRunner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+/*@RunWith(Cucumber.class)
 
 @CucumberOptions {
         features = {"src/test/java/Learning1/Features/"},
         glue = {"Learning1.Steps"}
-        }
+        }*/
 
 
 public class mainRunner {
 }
-*/
